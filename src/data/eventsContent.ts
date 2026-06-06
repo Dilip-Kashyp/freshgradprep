fe12227795287bc9a08fcd756d32160a46e9bc7f
vsamhita2028 <vsamhita051@gmail.com>
2026-06-06 13:47:50 +0530

    Removing Past Events Section

--- src/data/eventsContent.ts
+++ src/data/eventsContent.ts
@@ -35,17 +35,5 @@ export const eventsContent: EventsContent = {
   ],
   pastTitle: 'Past Events',
   pastEvents: [
-    {
-      name: 'How Hiring Actually Works (From the Other Side of the Table)',
-      date: '5th Apr, 2026',
-      timings: '4:00 PM - 6:00 PM',
-      mode: 'Offline',
-      venue: 'Lotus pond, Road Number 12, MLA Colony, Film Nagar, Hyderabad',
-      venueUrl: 'https://maps.app.goo.gl/QZvdomygVRrH68iw8',
-      registrationOpen: true,
-      registrationLink:
-        'https://www.goavo.ai/events/forms/fillup?id=69b2b0b44c6ae6af444e13d6',
-      registrationLabel: 'RSVP Open',
-    },
   ],
 };
